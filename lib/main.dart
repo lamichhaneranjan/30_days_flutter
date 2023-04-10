e078ea700f4bb23cc020e70838081c9ea991b06f
Ranjan <lamichhaneranjan1@gmail.com>
2023-04-10 21:19:42 +0545

    first day of github

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+            child: Container(
+      child: Text("Welcome to 30 days of flutter"),
+    )));
+  }
+}
