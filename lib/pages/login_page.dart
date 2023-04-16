c3ac87e35e13ca6c5716925d2b5e7f9b1bc6aea1
Ranjan <lamichhaneranjan1@gmail.com>
2023-04-16 17:21:39 +0545

    login image changed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,8 +6,32 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Column(
-      children: [Image.asset("assets/images/login_page.png")],
-    ));
+      child: Column(
+        children: [
+          Image.asset("assets/images/login_page1.png", fit: BoxFit.cover),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Enter UserName", labelText: "Username"),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                hintText: "Enter Password", labelText: "password"),
+          ),
+        ],
+      ),
+    );
   }
 }
